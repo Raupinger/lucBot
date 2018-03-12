a67e0be16f99b4bfc324e03c3981ef8e939ec4c5
Raupinger <fgrauper@gmail.com>
2018-03-12 20:27:49 +0100

    genaralized the requirement

--- discordSetup.js
+++ discordSetup.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const {token} = require('./config.json');
 
 client.on('ready', () => {
           console.log('Ready!');
           });
 
-client.login(config.token);
+client.login(token);
